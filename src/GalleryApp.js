edb37f1e2bf9a8136095c06787127f7dd18370af
josue <jovz1798@gmail.com>
2021-03-31 17:53:49 -0600

    sass and GalerryApp component setup

---
+++ src/GalleryApp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const GalleryApp = () => {
+    return (
+        <div>
+            <h1>GalleryApp</h1>
+            <p>I'm gonna build something beautiful</p>
+        </div>
+    )
+}
